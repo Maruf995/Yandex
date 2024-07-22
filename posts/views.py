da50a0b61d0a46bb8d6294ef86df230c547ce725
Maruf995 <mamadalievmaruf740@gmail.com>
2024-07-22 22:51:10 +0600

    added telegram bot

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
+import requests
+import os
+
+from dotenv import load_dotenv
+
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -7,15 +12,29 @@ from .serializers import PostSerializer, PostDetailSerializer, RatingSerializer,
     CommentSerializer
 from .permissions import IsPostOwnerOrStaff
 
+load_dotenv()
 
 class PostView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
+    
 class AddPostView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsPostOwnerOrStaff]
+    permission_classes = [IsPostOwnerOrStaff]  
+
+    def perform_create(self, serializer):
+        post = serializer.save(user=self.request.user)  
+        self.send_telegram_message(post.user.telegram, f"Ваш пост успешно опубликован.")
+
+    def send_telegram_message(self, chat_id, message):
+        token = os.getenv('TELEGRAM_BOT_TOKEN')
+        url = f'https://api.telegram.org/bot{token}/sendMessage'
+        payload = {
+            'chat_id': chat_id,
+            'text': message
+        }
+        requests.post(url, data=payload)
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
