35969024f05a8e0f0222a1a086174f4d01dfd9c1
Maruf995 <mamadalievmaruf740@gmail.com>
2024-07-22 20:51:09 +0600

    added docker

--- main/settings.py
+++ main/settings.py
@@ -10,17 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3+^e)fkg6=e_@-ol_lgvs+$994-w+m+_hmxuua2a6tx4(5*w19'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,8 +92,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
---
+++ readme.md
@@ -0,0 +1,25 @@
+## Технологии:
+Python 3.11, Django, DRF, Postgres, Docker
+
+### Запустить проект локально
+1. Скопировать проект
+```python
+git clone https://github.com/Maruf995/Yandex.git
+```
+
+2. Сделать миграции:
+```
+docker-compose run web python manage.py makemigrations
+docker-compose run web python manage.py migrate
+```
+3. Создать суперпользователя:
+```
+docker-compose run web python manage.py createsuperuser
+```
+4. Запустить проект:
+```
+docker-compose up
+```
+
+Бэкенд будет доступен по адресу `http://localhost`, `http://127.0.0.1`
+Админка будет доступена по адресу `http://localhost/admin`
